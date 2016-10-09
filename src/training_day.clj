62345218810349fa81870a8f7a55a1f05cd6cb65
Giedrius <giedrius.petrosius@ktu.edu>
2016-10-09 20:40:07 +0300

    Training day

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,17 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer 42)
 
-(def hai "O HAI!")
+(defn                                 ; Start a function definition:
+  hai                              ; name
+  "Gives out personalized greetings." ; a optional docstring
+  [who]                               ; parameters inside brackets
+  (str "Hello, " who "!"))
 
 (defn square [x]
-  ":(")
+  (* x x)
+   )
 
 (defn average [a b]
-  ":(")
+  (/ (+ a b) 2))
+
